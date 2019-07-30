1cf9002f36a4c8796cf273dbfb5d2b8ae1e508f7
haixiaShang <1553299260@qq.com>
2019-07-30 15:13:28 +0800

    hcc_code

---
+++ procedure/HCC_procedure/information_integration/ORIrm/mean_roc_new.R
@@ -0,0 +1,116 @@
+rm(list=ls())
+library(R.matlab)#D
+#setwd("D:/shx_bioinformatics/TCGA_Project/used_for_plot_ROC_and_static")
+#data <- readMat("reg_s_33_new.mat")
+setwd("D:/shx_bioinformatics/TCGA_Project/Copy_of_reg_mi")
+setwd("D:/git_data/procedure/HCC_procedure/information_integration/ORIrm")
+#data <- readMat("RDC_3_33_new.mat")
+#setwd("I:\\shx_bioinformatics\\TCGA_Project\\Copy_of_reg_c")
+data <- readMat("reg_m_33_new.mat")
+##直接把pr value 输入看看可否出来roc
+D <-as.data.frame(data[2])
+N_pr <-(data[["Normal.pr"]])
+D_pr <-(D[,"D.2"])
+index_all <-rbind()
+library(cutpointr)                                  
+#library(pROC)
+#tp_list <-rbind()
+#fp_list <-rbind()
+#Thre_list <-rbind()
+for(i in 1:dim(N_pr)[2])#
+{
+  #i <-1
+  data_t <- as.matrix(D_pr)
+  #length(data_t)
+  data_n <- as.matrix(N_pr[,i])
+  label<-matrix(c(rep(1,nrow(data_t)),rep(0,nrow(data_n))))
+  ##疾病标1正常标0
+  #label
+  use <-c(data_t,data_n)
+  # k1 <-roc(as.factor(label),as.vector(use),plot=T,print.auc=T,print.thres=T,col="blue",lty=1,lwd=2)
+  #ll1 <-t(as.matrix(k1[["specificities"]]))
+  #ll1
+  #length(ll1)
+  #dim(ll1)
+  #SE_list <-rbind(SE_list,t(as.matrix(k1[["specificities"]])))
+  #SP_list <-rbind(SP_list,t(as.matrix(k1[["sensitivities"]])))
+  #Thre_list <-rbind(Thre_list,t(as.matrix(k1[["thresholds"]])))
+  #k1[["specificities"]]
+  opt_cu <- cutpointr(x =as.vector(use) , class = as.vector(label))
+  opt_cu
+  zz <-opt_cu[["roc_curve"]]
+  z1 <-zz[[1]]
+  #mode(z1)
+  #tp_list <-rbind(tp_list,t(as.matrix(z1[["tpr"]])))
+  #fp_list <-rbind(fp_list,t(as.matrix(z1[["fpr"]])))
+  acc <-round(opt_cu[["acc"]],4)
+  se <-round(opt_cu[["specificity"]],4)
+  sp <-round(opt_cu[["sensitivity"]],4)
+  F1 <-round(2*(se*sp)/(sp+se),4)
+  auc <-round(opt_cu[["AUC"]],4)
+  used_index <-cbind(se,sp,acc,F1,auc)
+  index_all<-rbind(index_all,used_index)
+}
+dim(index_all)
+sd(index_all)
+mm <-cbind()
+sdd <-cbind()
+for(i in 1:dim(index_all)[2])
+{
+   mm <-cbind(mm,round(mean(index_all[,i]),4))
+  sdd <-cbind(sdd,round(sd(index_all[,i]),4))
+}
+info <-c("Se","Sp","acc","F1","AUC")
+info
+mm
+sdd
+
+if(F)
+{
+#library(rapport)
+va <-rbind()
+xx <-rbind()
+mm <-rbind()
+mn <-rbind()
+#lk <-unique(Thre_list)
+sd <-seq(0,1,by=1/60)
+for(i in 1:length(sd))
+{
+  #画格子 
+  #落在0~0.001之间的所有点的se的均值
+  #i <-0
+  # i <-1
+  l1 <-which(fp_list<sd[i+1])
+  l2 <-which(fp_list>=sd[i])
+  in1 <-intersect(l1,l2)
+  ##若是非空
+  if(length(in1)>0)
+  {
+  mm1 <-max(tp_list[in1])
+  mn1 <-min(tp_list[in1])
+  #va <-rbind(va,mean(SE_list[l1]))
+  mm <-rbind(mm,mm1)
+  mn <-rbind(mn,mn1)
+  va <-rbind(va,(mn1+(mm1-mn1)/2))
+  xx <-rbind(xx,sd[i])
+  }
+}
+length(xx)
+length(va)
+plot(c(0,xx),c(0,va),type = 'l',col = "red",lty=4,ylim = c(0.0,1),xlab = "False positive rate",
+     ylab = "True Positive rate")
+library(ggplot2)
+x11 <-c(0,xx)
+yy1 <-c(0,va)
+yy2 <-c(0,mn)
+yy3 <-c(0,mm)
+data <-data.frame(x11,yy1,yy2,yy3)
+ggplot(data,aes(x=x11,y=yy1))+
+  geom_line(colour = "blue",size=1)+
+  geom_ribbon(aes(ymin=yy2,ymax=yy3),alpha=0.2,fill="red")+
+  #labs(x="1 - Specificity", y="Sensitivity", colour="")+,axis.title.x=element_blank()
+  xlab("1-Specificity")+theme(axis.text.x = element_text(colour = 'black', angle = 0, size = 13, hjust = 0.5, vjust = 0.5)) + 
+  ylab("Sensitivity") + theme(axis.text.y = element_text(colour = 'black', size = 12), axis.title.y = element_text(size = 12, hjust = 0.5, vjust = 0.2)) + 
+  theme(strip.text.y = element_text(size = 11, hjust = 0.0, vjust = 0.2, face = 'bold'))
+##这个图可以了
+}
---
+++ procedure/HCC_procedure/information_integration/ORIrm/reg_mi.m
@@ -0,0 +1,159 @@
+clear;clc;close all;
+%% gene level 的信息
+G_data=importdata('data_used_rna_10_22.txt');
+used_G=G_data.data(:,[1,2,4]);
+used_G=used_G(find(used_G(:,3)>0),:);
+Sg=unique(used_G(:,1));
+Tg=unique(used_G(:,2));
+Go_node=unique(union(Sg,Tg));
+% 加载miRNA 信息
+M_data=importdata('TarBase_with_miRNA_new.txt');
+used_M=M_data.data(:,[1,2,4]);
+used_M=used_M(find(used_M(:,3)>0),:);
+Sm=unique(used_M(:,1));
+Tm=unique(used_M(:,2));
+Mo_node=unique(union(Sm,Tm));
+%% Reg level Transition Matrix Construction
+Reg_node=unique(union(Go_node,Mo_node));
+NR=length(Reg_node);
+R=sparse(NR,NR);
+RW=sparse(NR,NR);
+RCS=sparse(NR,NR);
+% 加入gene regulation 信息
+for(i=1:length(used_G))
+    l1=find(used_G(i,1)==Reg_node);
+    l2=find(used_G(i,2)==Reg_node);
+    if(~isempty(l1))
+        if(~isempty(l2))
+            R(l2,l1)=1;
+            RW(l2,l1)=used_G(i,3);
+            RCS(l2,l1)=1;
+        end
+    end
+end
+length(find(RW>0))
+% 加入miRNA regulation 
+for(i=1:length(used_M))
+    l1=find(used_M(i,1)==Reg_node);
+    l2=find(used_M(i,2)==Reg_node);
+    if(~isempty(l1))
+        if(~isempty(l2))
+            R(l2,l1)=1;
+            RW(l2,l1)=used_M(i,3);
+            RCS(l2,l1)=1;
+        end
+    end
+end
+% normalize transition matrix
+sum_R=sum(R,1);
+for(i=1:length(sum_R))
+    if(sum_R(i)>0)
+    R(:,i)= R(:,i)/sum_R(i);
+    end
+end
+% weighing transition matrix
+RTW=R.*RW;
+%% 构建总体的转移矩阵
+Node_all=[Reg_node];
+number=length(Node_all);
+Trans=sparse(number,number);
+Trans(1:NR,1:NR)=RTW;
+
+CS=sparse(number,number);
+CS(1:NR,1:NR)=RCS;
+%% input CPR
+r=0.85;
+threshold=1e-10;%
+N = length(Trans);
+PR =( 1/N*ones(N,1));%
+restart =PR;
+iter = 1;
+delta_PR = Inf; 
+while (delta_PR > threshold || iter>200)    %iterate until error is less than 1e-10 1e-7
+    tic;
+    prev_PR = PR;               %save previous PageRank vector (t-1)
+    CST=CS.*(1*Trans);%*1/N Trns'
+    CST(find(isnan(CST)==1))=0;
+    PR = r*CST* PR + (1-r)*restart;     %calculate new Pa
+    delta_PR= norm(PR-prev_PR);%calculate new error
+    t(iter)=toc;
+    iter = iter + 1;
+end
+length(unique(PR))
+[Rank,index]=sort(unique(PR'),'descend');%
+rank=1:length(unique(PR));
+%% gene  rank in RegNetwork 中的排序
+[LRPR,LRrank]=sort(unique(PR(1:NR)),'descend');
+length(unique(LRPR))
+% gain information of disease genes
+disease = importdata('hcc_disease_genes.txt');
+HD = disease.data;
+D=[]; ii=1;
+for(i=1:length(HD))
+    l1=find((HD(i)+0.4)==Reg_node);
+    l11=find((HD(i)+0.4)==unique(used_M(:,2)));
+    if(~isempty(l1))
+        if(~isempty(l11))
+              D(ii,1)=HD(i);
+              D(ii,2)=PR(l1);
+              D(ii,3)=find(PR(l1)==LRPR);
+                  ii=ii+1;
+          end
+    end
+end
+Auc=[];
+Normal_rank=[];
+Normal_pr=[];
+Normal_id=[];
+Circle=1000;
+M_num=unique(used_M(:,2));
+normal_genes_all=M_num-0.4;
+Candidate_normal=setdiff(normal_genes_all,D(:,1));
+
+for(count=1:Circle)
+Rand_loc_p=randperm(length(Candidate_normal),length(D));
+Rand_gene_ID=Candidate_normal(Rand_loc_p);
+nor=[];ii=1;
+for(i=1:length(Rand_loc_p))
+    l1=find((Rand_gene_ID(i)+0.4)==Reg_node);
+    if(~isempty(l1))
+        nor(ii,1)=Rand_gene_ID(i);
+        nor(ii,2)=PR(l1);
+        nor(ii,3)=find(PR(l1)==LRPR);
+        ii=ii+1;
+    end
+end
+% 存储一下rank
+for(j=1:length(nor))
+Normal_rank(j,count)=nor(j,3);
+Normal_pr(j,count)=nor(j,2);
+Normal_id(j,count)=nor(j,1);
+end
+nor_rank_f=nor(:,3);
+D_rank_f=D(:,3);
+data_p=[];
+for(i=1:(2*(length(D_rank_f))))
+    if(i<=length(D_rank_f))
+   data_p(i,1) =D_rank_f(i);%disease 
+    else
+       data_p(i,1) = nor_rank_f(i-length(D_rank_f)); % normal 
+    end
+end
+y_p=(1:(2*(length(D_rank_f))))'<(length(D_rank_f));%>
+%
+mdlSVM = fitcsvm(data_p,y_p,'Standardize',true);
+mdlSVM = fitPosterior(mdlSVM);
+[~,score_svm] = resubPredict(mdlSVM);
+[Xsvm,Ysvm,Tsvm,AUCsvm] = perfcurve(y_p,score_svm(:,mdlSVM.ClassNames),'true');
+Auc(count)=AUCsvm;
+end
+boxplot(Auc)
+max(Auc)
+mean(Auc)
+min(Auc)
+length(find(Auc>0.7))
+length(find(Auc<0.6))
+std(Auc)
+% save reg_m_33
+median(Auc)
+save('reg_m_33_new.mat','D','Auc','Normal_id','Normal_pr','Normal_rank','-v6')
